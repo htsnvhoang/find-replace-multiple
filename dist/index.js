d66a49de09c790dde3c131866be04bfc545ddd6f
hoangnv2nf <hoangnv2nf@gmail.com>
2023-07-21 10:48:12 +0700

    test find v2

--- dist/index.js
+++ dist/index.js
@@ -5295,57 +5295,42 @@ async function getFilePath(fileInput) {
     if (!files || !files.length) return null;
     return files[0]
 }
-function getListString(str) {
-    const separator = core.getInput('separator') || ",";
-    if (typeof str == 'string') {
-        return str.split(separator).filter(Boolean).map(_s => _s.trim());
-    }
-    return ''
-}
-function transformString(str, key) {
-    const _str = str.trim();
-    if (_str[0] == "'" || _str[0] == '"') {
-        console.log("Transform string key --> ", key);
-        return _str.substring(1, _str.length - 1)
-    }
-    return str
-}
+
 async function main() {
     try {
         const paramInput = core.getInput('params', { required: true });
         const data = JSON.parse(paramInput);
-        console.log("typeof data ", typeof data);
-        console.log("data ", data);
-        // const finds = getListString(core.getInput('finds'));
-        // const replaces = getListString(core.getInput('replaces'));
-        // const isTransform = core.getBooleanInput('isTransform');
+        if (typeof data != 'object' || !data['finds'] || !data['replaces']) {
+            core.setFailed("Invalid input");
+            return;
+        }
+        const { finds, replaces } = data;
+        if (!finds.length || !replaces.length) {
+            core.setFailed("Empty input finds and replacse");
+            return;
+        }
 
-        // if (!finds || !replaces) {
-        //     core.setFailed("Invalid input");
-        //     return;
-        // }
+        const filePathInclude = await getFilePath(core.getInput('include'));
+        if (!filePathInclude) {
+            core.setFailed("Invalid file path include");
+            return;
+        }
 
-        // const filePathInclude = await getFilePath(core.getInput('include'));
-        // if (!filePathInclude) {
-        //     core.setFailed("Invalid file path include");
-        //     return;
-        // }
+        const fileBuffer = fs.readFileSync(filePathInclude);
+        const fileContent = fileBuffer.toString();
+        let newContent = fileContent;
 
-        // const fileBuffer = fs.readFileSync(filePathInclude);
-        // const fileContent = fileBuffer.toString();
-        // let newContent = fileContent;
+        finds.forEach((str, i) => {
+            let _val = replaces[i];
+            if (str && _val) {
+                newContent = newContent.replace(str, _val);
+                console.log("Replace key --> ", str)
+            }
+        })
+        console.log("New content --> ", newContent)
 
-        // finds.forEach((str, i) => {
-        //     let _val = replaces[i];
-        //     if (str && _val) {
-        //         if (isTransform) _val = transformString(_val, str);
-        //         newContent = newContent.replace(str, _val);
-        //         console.log("Replace key --> ", str)
-        //     }
-        // })
-
-        // fs.writeFileSync(filePathInclude, newContent);
-        // core.info("Find and replace success !!!")
+        fs.writeFileSync(filePathInclude, newContent);
+        core.info("Find and replace success !!!")
     } catch (error) {
         core.setFailed(error.message);
     }
