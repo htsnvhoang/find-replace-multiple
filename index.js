462246a0aa273d001c4c4aad8744a883668e440b
hoangnv2nf <hoangnv2nf@gmail.com>
2023-07-21 11:56:11 +0700

    fix

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ async function findReplace(filePath, finds, replaces) {
         }
     })
 
-    fs.writeFileSync(filesPathInclude, newContent);
+    fs.writeFileSync(filePath, newContent);
     core.info("Find and replace success !!!");
     core.endGroup();
 }
